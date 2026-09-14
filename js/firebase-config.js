e7ce381ea0a7823905cfead4ac97afbe85266d0f
Manisha M <manishaseervi2407@gmail.com>
2026-09-14 12:04:45 +0530

    Update Firebase database URL

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -12,7 +12,7 @@
 // ============================================================
 
 const firebaseConfig = {
-  databaseURL: "https://firebaseio.com",
+  databaseURL: "https://safepath-ai-web-default-rtdb.firebaseio.com",
   apiKey: "AIzaSyAck8OHUco9-_7Pi566nh2avOyMHz-SdaI",
   authDomain: "safepath-ai-web.firebaseapp.com",
   projectId: "safepath-ai-web",
